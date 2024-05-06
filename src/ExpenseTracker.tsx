600313cb82e70ca092b9261a86f2ebd1bbec7fb1
KajtekRut <145433455+KajtekRut@users.noreply.github.com>
2024-05-06 11:00:44 +0200

    Update ExpenseTracker.tsx

--- src/ExpenseTracker.tsx
+++ src/ExpenseTracker.tsx
@@ -43,7 +43,7 @@ const ExpenseTracker: React.FC = () => {
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         try {
-            const response = await fetch("http://adres-twojego-backendu/api/expenses", {
+            const response = await fetch("http://localhost:3000", {
                 method: "POST",
                 headers: {
                     "Content-type": "application/json",
